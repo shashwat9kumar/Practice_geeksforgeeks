6c94e97f9c6c4300ac80f233ed0ba5a220c1be34
shashwat9kumar <shashwat9kumar@gmail.com>
2020-04-22 22:16:58 +0530

    Question : Given a number N and the task is to print the first and last digit of N.

--- Corner_Digits/Corner_Digits.java
+++ Corner_Digits/Corner_Digits.java
@@ -5,11 +5,13 @@ import java.io.*;
 class Corner_Digits {
 	public static void main (String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+		System.out.println("Enter the number of test cases :");
 		int t = sc.nextInt();
 		
 		while(t-- > 0)
 		{
+			
+			System.out.println("Enter a number : ");
 		    String num = sc.next();
 		    try{
 		        char f = num.charAt(0);
