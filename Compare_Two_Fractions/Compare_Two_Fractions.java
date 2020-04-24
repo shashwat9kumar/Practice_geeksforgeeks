65229238b954a6976a77b808f0e84b6a14a54b06
shashwat9kumar <shashwat9kumar@gmail.com>
2020-04-24 21:56:48 +0530

    Question : Given two fractions a/b and c/d, compare them and print the larger of the two.
    Input:
    The first line of the input contains a single integer T, denoting the number of test cases. Then T test case follows. Each testcase contains a single line having a/b and c/d as input. a/b and c/d are separated by a comma and a space.
    
    Example:
    
    Input:
    3
    5/6, 11/45
    8/1, 8/1
    2/3, 2/2
    
    Output:
    5/6
    equal
    2/2

---
+++ Compare_Two_Fractions/Compare_Two_Fractions.java
@@ -0,0 +1,72 @@
+import java.util.*;
+
+class Compare_Two_Fractions
+{
+	
+	public static void main(String args[])
+	{
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int t = sc.nextInt();
+		
+		while(t-- > 0)
+		{
+			
+			String x = sc.next();
+			String y = sc.next();
+			
+			
+			
+			
+			
+			
+			String str1[] =x.split("/",2);
+			String str2[] =y.split("/",2);
+			
+			
+			
+			
+			
+			str1[1]= str1[1].substring(0, (str1[1].length()-1));
+			
+			
+			
+			double n1 = Double.parseDouble(str1[0]);
+			double n2 = Double.parseDouble(str2[0]);
+			
+			double d1 = Double.parseDouble(str1[1]);
+			double d2 = Double.parseDouble(str2[1]);
+			
+			
+			double f1 = n1/d1;
+			double f2 = n2/d2;
+			
+			if(f1 > f2)
+			{
+				System.out.println(str1[0]+"/"+str1[1]);
+			}
+			else if(f2 > f1)
+			{
+				System.out.println(str2[0]+"/"+str2[1]);
+			}
+			else
+			{
+				System.out.println("equal");
+			}
+			
+			
+			/*
+			System.out.println(str1[0]);
+			System.out.println(str1[1]);
+			
+			
+			System.out.println(str2[0]);
+			System.out.println(str2[1]);
+			
+			*/
+		}
+		
+	}
+	
+}
