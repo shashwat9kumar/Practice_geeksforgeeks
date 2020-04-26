366916c9bbbba6369cf6ad09ec81599107ad0a5a
shashwat9kumar <shashwat9kumar@gmail.com>
2020-04-26 23:02:55 +0530

    Question : You are given an n x n 2D matrix representing an image.
    Rotate the image by 90 degrees (clockwise).
    
    You need to do this in place.
    
    Note that if you end up using an additional array, you will only receive partial score.

---
+++ Data_Structures/Arrays/Rotate_2D_Array/Rotate_2D_Array.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int t;
+	cin>>t;
+	while(t-- > 0)
+	{
+	    int n;
+	    cin>>n;
+	    int arr[n][n];
+	    
+	    for(int i=0;i<n;i++)
+	    {
+	        for(int j=0;j<n;j++)
+	        {
+	            cin>>arr[i][j];
+	        }
+	    }
+	    
+	    for(int i=0;i<n;i++)
+	    {
+	        for(int j=n-1;j>=0;j--)
+	        {
+	            cout<<arr[j][i]<<" ";
+	        }
+	    }
+	    cout<<endl;
+	}
+	
+	
+	
+	return 0;
+}
