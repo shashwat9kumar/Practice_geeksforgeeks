de9438ce09a06eaf0e4dc4e3561c8b647c97a3fa
shashwat9kumar <shashwat9kumar@gmail.com>
2020-04-26 22:08:28 +0530

    Question : Given an unsorted array arr[] of size N, rotate it by D elements (clockwise).

--- Data_Structures/Arrays/Rotate_Arrays/Rotate_Arrays.cpp
+++ Data_Structures/Arrays/Rotate_Arrays/Rotate_Arrays.cpp
@@ -5,15 +5,20 @@ int main() {
 	
 	
 	int t;
+	cout<<"Enter the number of test cases : "<<endl;
 	cin>>t;
 	
 	while(t-- > 0)
 	{
 	    int n,x;
+		cout<<"Enter the number of array elements and the number of items to rotate the array by : "<<endl;
 	    cin>>n>>x;
 	    
 	    int arr[n];
 	    
+		
+		cout<<"Enter the elements of array : "<<endl;
+		
 	    for(int i=0;i<n;i++)
 	    {
 	        cin>>arr[i];
